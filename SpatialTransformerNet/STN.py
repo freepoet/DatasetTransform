d44ec842a1e96b1f59282a86c0ceb99b05155f2d
root <ningmq@qq.com>
2021-07-07 16:44:30 +0800

    test

--- SpatialTransformerNet/STN.py
+++ SpatialTransformerNet/STN.py
@@ -34,20 +34,20 @@ test_loader = torch.utils.data.DataLoader(
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
-        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
-        self.conv2_drop = nn.Dropout2d()
-        self.fc1 = nn.Linear(320, 50)
-        self.fc2 = nn.Linear(50, 10)
+        self.conv1 = nn.Conv2d(1, 10, kernel_size=5) #   64 1 28 28    64 10 24 24   64 10 12 12
+        self.conv2 = nn.Conv2d(10, 20, kernel_size=5) #  64 10 12 12   64 20  8  8   64 20  4  4
+        self.conv2_drop = nn.Dropout2d() #
+        self.fc1 = nn.Linear(320, 50) #
+        self.fc2 = nn.Linear(50, 10) #
 
         # Spatial transformer localization-network
         self.localization = nn.Sequential(
-            nn.Conv2d(1, 8, kernel_size=7),
-            nn.MaxPool2d(2, stride=2),
-            nn.ReLU(True),
-            nn.Conv2d(8, 10, kernel_size=5),
-            nn.MaxPool2d(2, stride=2),
-            nn.ReLU(True)
+            nn.Conv2d(1, 8, kernel_size=7), # 64 1 28  28  64 8 22 22
+            nn.MaxPool2d(2, stride=2),#        64 8 22 22  64 8 11 11
+            nn.ReLU(True),#
+            nn.Conv2d(8, 10, kernel_size=5),#   64 8 11 11   64 10 7 7
+            nn.MaxPool2d(2, stride=2),#     64 10 7 7        64 10 3 3
+            nn.ReLU(True)#
         )
 
         # Regressor for the 3 * 2 affine matrix
@@ -79,9 +79,9 @@ class Net(nn.Module):
         x = self.stn(x)  #[64, 1, 28, 28]->[64, 1, 28, 28])
 
         # Perform the usual forward pass
-        x = F.relu(F.max_pool2d(self.conv1(x), 2))
-        x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
-        x = x.view(-1, 320)
+        x = F.relu(F.max_pool2d(self.conv1(x), 2))  #64 10 12 12
+        x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))  # 64 20  4  4
+        x = x.view(-1, 320)  #  [64, 320]
         x = F.relu(self.fc1(x))
         x = F.dropout(x, training=self.training)
         x = self.fc2(x)
